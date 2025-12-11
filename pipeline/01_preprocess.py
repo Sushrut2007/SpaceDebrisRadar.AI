b39fa335f2a30d799f9f699d74e01ed814345c2f
Sushrut <sushrutdeshmukh4@gmail.com>
2025-12-11 17:01:17 +0530

    Created preprocessing.py file

---
+++ pipeline/01_preprocess.py
@@ -0,0 +1,12 @@
+"""
+* Folder - pipeline
+* Folder purpose - Acts as a backend layer for Streamlit pages.
+* File - 01_preprocess.py
+* File purpose - Handle preprocessing stage, including : 
+    1. Load fresh dataset.
+    2. Drop duplicates, missing rows.
+    3. Create new features.
+    4. Drop features not required by ML models.
+    5. Encode categorical featrues and scale the dataset.
+
+"""
