b0ac955e581ea48be0a0e440cf4b87e066904701
Sushrut <sushrutdeshmukh4@gmail.com>
2025-12-15 16:49:36 +0530

    Implemented function to train KMeans clustering model

--- pipeline/02_clustering.py
+++ pipeline/02_clustering.py
@@ -14,5 +14,30 @@
 import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans
+import joblib
+from pipeline.utils import fetch_dataset
 
 
+
+def run_KMeans(df):
+    """
+    Train KMeans algorithm to cluster related rows into groups of similar satellites.\n
+    Uses: k = 5\n
+    Also saves the KMeans model for future use.
+
+    Args:
+        df (DataFrame): Dataframe with 'clusterable' columns
+    
+    Returns: 
+        None
+    """
+
+    model = KMeans(n_clusters=5, random_state=101)
+    # Train and test the model
+    cluster_labels = model.fit_predict(df)
+
+    # Make a seperate column
+    df['CLUSTER'] = cluster_labels
+
+    # Save the model as .joblib
+    joblib.dump(model, '../models/clustering.joblib')
